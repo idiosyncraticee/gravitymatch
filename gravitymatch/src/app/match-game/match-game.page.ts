6190ec0cf367a4a8001bbef90159f306c6983c26
idiosyncraticee <chris.schuermyer@gmail.com>
2018-11-18 15:10:16 -0800

    Done

--- gravitymatch/src/app/match-game/match-game.page.ts
+++ gravitymatch/src/app/match-game/match-game.page.ts
@@ -9,7 +9,21 @@ import { ClientProfilePage } from '../client-profile/client-profile.page';
 })
 export class MatchGamePage implements OnInit {
    randos:any;
-   star_color:any;
+   rando1:any;
+   rando1_name:any="Patrick";
+   rando1_location:any="Spokane, WA";
+   rando1_description:any="Triceratops breeder and amature water purifier.";
+   rando2_name:any="Scott";
+   rando2_location:any="Spokane, WA";
+   rando2_description:any="Triple jointed luger.";
+   rando3_name:any="Ryan";
+   rando3_location:any="Spokane, WA";
+   rando3_description:any="Able designer and cat babysitter.";
+rando2:any;
+rando3:any;
+   star_color1:any;
+   star_color2:any;
+   star_color3:any;
 
    constructor( private modalCtrl:ModalController,private alertCtrl:AlertController) {}
    async openModal()
@@ -17,7 +31,7 @@ export class MatchGamePage implements OnInit {
      const modal = await this.modalCtrl.create({
       component: ClientProfilePage
     });
- 
+
     return await modal.present();
    }
 
@@ -34,23 +48,39 @@ export class MatchGamePage implements OnInit {
          { name: "Joe", location: "22 miles", mm: '12', rating: "4", show: true },
          { name: "Patrick", location: "22 miles", mm: '12', rating: "4", show: true },
       ];
-
-      this.star_color = [
+      this.rando1 =
+         { name: "Ryan", location: "22 miles", mm: '12', rating: "4", show: true, description: "Saltwater aquarium enthusiast ... nay, zealot." };
+      this.rando2 =
+         { name: "Patricia", location: "22 miles", mm: '12', rating: "4", show: true, description: "Saltwater aquarium enthusiast ... nay, zealot." };
+      this.rando3 =
+         { name: "Chris", location: "22 miles", mm: '12', rating: "4", show: true, description: "Saltwater aquarium enthusiast ... nay, zealot." };
+      this.star_color1 = [
          {rating: 0, color: "#00FF00", show: false},
          {rating: 1, color: "#00FF00", show: false},
          {rating: 2, color: "#00FF00", show: false},
          {rating: 3, color: "#00FF00", show: false},
          {rating: 4, color: "#00FF00", show: false}
       ];
-   }
+      this.star_color2 = [
+         {rating: 0, color: "#00FF00", show: false},
+         {rating: 1, color: "#00FF00", show: false},
+         {rating: 2, color: "#00FF00", show: false},
+         {rating: 3, color: "#00FF00", show: false},
+         {rating: 4, color: "#00FF00", show: false}
+      ];
+      this.star_color3 = [
+         {rating: 0, color: "#00FF00", show: false},
+         {rating: 1, color: "#00FF00", show: false},
+         {rating: 2, color: "#00FF00", show: false},
+         {rating: 3, color: "#00FF00", show: false},
+         {rating: 4, color: "#00FF00", show: false}
+      ];
 
 
-   doSwipe(rando) {
-      console.log(rando);
-      console.log("siwped");
-   }
+}
 
-   star(rating, item, slidingItem) {
+
+   star3(rating, item, slidingItem) {
 
 
       //await delay(300);
@@ -59,18 +89,19 @@ export class MatchGamePage implements OnInit {
       console.log(slidingItem);
       console.log(rating);
       const index = this.randos.indexOf(item);
-      if (index > -1) {
-         this.randos.splice(index, 1);
-         this.randos[index].show=false;
-      }
+      //if (index > -1) {
+      //   this.randos.splice(index, 1);
+      //   this.randos[index].show=false;
+      //}
       for(var i = 0;i<5;i++) {
 
-            this.star_color[i].show=false;
+
+            this.star_color3[i].show=false;
 
       }
       for(var i = 0;i<5;i++) {
          if(i<=rating.rating) {
-            this.star_color[i].show=true;
+            this.star_color3[i].show=true;
          }
       }
 
@@ -82,16 +113,70 @@ export class MatchGamePage implements OnInit {
       //slidingItem.close();
    }
 
-   //async function delay(ms: number) {
-   //    return new Promise( resolve => setTimeout(resolve, ms) );
-   //}
+   star2(rating, item, slidingItem) {
 
-   presentModal() {
-   //   const modal = await this.modalController.create({
-   //     component: ClientProfilePage,
-   //     componentProps: { value: 123 }
-   //   });
-   //   return await modal.present();
+
+      //await delay(300);
+      console.log("rated");
+      console.log(item);
+      console.log(slidingItem);
+      console.log(rating);
+      const index = this.randos.indexOf(item);
+      //if (index > -1) {
+      //   this.randos.splice(index, 1);
+      //   this.randos[index].show=false;
+      //}
+      for(var i = 0;i<5;i++) {
+            this.star_color2[i].show=false;
+
+      }
+      for(var i = 0;i<5;i++) {
+         if(i<=rating.rating) {
+            this.star_color2[i].show=true;
+         }
+      }
+
+
+      //this.star_color[rating].color("#FFFF00");
+
+
+      console.log("Following splice");
+      //slidingItem.close();
    }
+
+   star1(rating, item, slidingItem) {
+
+
+      //await delay(300);
+      console.log("rated");
+      console.log(item);
+      console.log(slidingItem);
+      console.log(rating);
+      const index = this.randos.indexOf(item);
+      //if (index > -1) {
+      //   this.randos.splice(index, 1);
+      //   this.randos[index].show=false;
+      //}
+      for(var i = 0;i<5;i++) {
+
+            this.star_color1[i].show=false;
+
+
+      }
+      for(var i = 0;i<5;i++) {
+         if(i<=rating.rating) {
+            this.star_color1[i].show=true;
+         }
+      }
+
+
+      //this.star_color[rating].color("#FFFF00");
+
+
+      console.log("Following splice");
+      //slidingItem.close();
+   }
+
+
 
 }
