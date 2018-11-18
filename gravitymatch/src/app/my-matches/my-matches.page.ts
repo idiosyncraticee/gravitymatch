9ce0ebc64b8fe443b12e6651d52a4bd4519adb8c
idiosyncraticee <chris.schuermyer@gmail.com>
2018-11-17 22:26:53 -0800

    Working on my-matches

--- gravitymatch/src/app/my-matches/my-matches.page.ts
+++ gravitymatch/src/app/my-matches/my-matches.page.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NavController } from '@angular/core';
+   import { Router } from '@angular/router';
+
+
+import { ClientProfilePage } from '../../client-profile/client-profile';
 
 @Component({
   selector: 'app-my-matches',
@@ -7,9 +11,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MyMatchesPage implements OnInit {
 
-  constructor() { }
+  constructor(
+          private router: Router,
+  ) { }
+
+   matches =   [
+      { name: "Ryan", location: "22 miles", mm: '12', rank: "4" },
+      { name: "Robbie", location: "22 miles", mm: '12', rank: "4" },
+      { name: "Kyle", location: "22 miles", mm: '12', rank: "4" },
+      { name: "Chris", location: "22 miles", mm: '12', rank: "4" },
+      { name: "Patricia", location: "22 miles", mm: '12', rank: "4" },
+      { name: "Joe", location: "22 miles", mm: '12', rank: "4" },
+      { name: "Patrick", location: "22 miles", mm: '12', rank: "4" },
+   ];
+
+
+
 
   ngOnInit() {
+
+
   }
+
+  approve(itemId) {
+
+     this.router.navigate(['/login', { id: itemId }]);
+
+ }
+ decline(itemId) {
+    console.log(itemId);
+    
+    //this.navCtrl.navigateRoot('/tabs/(account:account/login)');
+}
+
+
 
 }
