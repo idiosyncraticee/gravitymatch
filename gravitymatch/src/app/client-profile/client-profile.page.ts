58fb09c7a99b7802bc2eee6273eb8ee0a857f375
kws0307 <kyle.schuermyer@hotmail.com>
2018-11-18 12:15:05 -0600

    Judge Profiles

--- gravitymatch/src/app/client-profile/client-profile.page.ts
+++ gravitymatch/src/app/client-profile/client-profile.page.ts
@@ -13,13 +13,12 @@ export class ClientProfilePage implements OnInit {
 
   ngOnInit() {
      this.clients =   [
-       { name: "Chip", location: "Spokane, WA", mm: '12', rank: "4", show: "yes", headshot1: "./assets/Chip Overstreet.png",headshot2: "./assets/Chip Overstreet.png",headshot3: "./assets/Chip Overstreet.png", description: "Masters Degree in Falconry Sciences" },
-       { name: "Robbie", location: "22 miles", mm: '12', rank: "4", show: "yes" },
-       { name: "Kyle", location: "22 miles", mm: '12', rank: "4", show: "yes" },
-       { name: "Chris", location: "22 miles", mm: '12', rank: "4", show: "yes" },
-       { name: "Patricia", location: "22 miles", mm: '12', rank: "4", show: "yes" },
-       { name: "Joe", location: "22 miles", mm: '12', rank: "4", show: "yes" },
-       { name: "Patrick", location: "22 miles", mm: '12', rank: "4", show: "yes" },
+       { name: "Ryan", location: "Spokane, WA", mm: '12', rank: "8", show: "yes", headshot1: "./assets/Ryan Arnold.png",headshot2: "./assets/Ryan Arnold.png",headshot3: "./assets/Ryan Arnold.png", description: "Once got three pop-tarts in a single package" },
+         { name: "Pat", location: "Spokane, WA", mm: '12', rank: "10", show: "yes", headshot1: "./assets/Pat Atwal.png",headshot2: "./assets/Pat Atwal.png",headshot3: "./assets/Pat Atwal.png", description: "Extraordinarily fast rapper" },
+              { name: "Nadine", location: "Spokane, WA", mm: '12', rank: "6", show: "yes", headshot1: "./assets/Nadine Burgess.png",headshot2: "./assets/Nadine Burgess.png",headshot3: "./assets/Nadine Burgess.png", description: "Proud owner of industrial-strength dog washer" },
+                   { name: "Chip", location: "Spokane, WA", mm: '12', rank: "7", show: "yes", headshot1: "./assets/Chip Overstreet.png",headshot2: "./assets/Chip Overstreet.png",headshot3: "./assets/Chip Overstreet.png", description: "Honest, trustworthy and a great liar" },
+                        { name: "Steve", location: "Spokane, WA", mm: '12', rank: "8", show: "yes", headshot1: "./assets/Steve Trabun.png",headshot2: "./assets/.Steve Trabun.png",headshot3: "./assets/Steve Trabun.png", description: "Thinks chickens aren't real" },
+
      ];
 
      this.client = this.clients[0];
